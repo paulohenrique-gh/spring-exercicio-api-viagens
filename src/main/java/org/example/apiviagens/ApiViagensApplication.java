e4cbd90d67d5339a2a7d9489792913d1f29490ba
Paulo Henrique <ph.1928@hotmail.com>
2024-08-08 21:56:57 -0300

    Commit inicial

---
+++ src/main/java/org/example/apiviagens/ApiViagensApplication.java
@@ -0,0 +1,13 @@
+package org.example.apiviagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiViagensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiViagensApplication.class, args);
+    }
+
+}
